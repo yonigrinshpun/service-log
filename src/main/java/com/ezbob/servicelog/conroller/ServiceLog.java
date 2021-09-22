f3e61235aad577852208246a9bf10baee5ce358a
Yoni Grinshpun <yonig@yaelgroup.com>
2021-09-22 15:11:49 +0300

    Last ver

--- src/main/java/com/ezbob/servicelog/conroller/ServiceLog.java
+++ src/main/java/com/ezbob/servicelog/conroller/ServiceLog.java
@@ -1,16 +1,22 @@
 package com.ezbob.servicelog.conroller;
 
-import lombok.extern.java.Log;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@Log
 public class ServiceLog {
+    private static final Logger log = LoggerFactory.getLogger(ServiceLog.class);
+
     @PostMapping("/service-log")
-    public String serviceLog(@RequestBody String logValue){
-        log.info("---Yoni log: " + logValue);
-        return "OK";
+    public void serviceLog(@RequestBody String logValue){
+        log.info("---EziBob log: " + logValue);
+    }
+
+    @PostMapping("/service-error-log")
+    public void serviceErrLog(@RequestBody String logValue){
+        log.error("---EziBob log: " + logValue);
     }
 }
